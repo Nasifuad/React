eee2f9b9b84bbf269e17aa871a5345169d1feccb
Nasif Fuad <nasifuad007@gmail.com>
2024-10-29 09:15:50 +0600

    vv

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import TodoInput from "./components/todoInput";
 const TodoApp = () => {
   return (
     <div className="bg-slate-900 h-screen w-screen  flex flex-col items-center justify-center ">
-      <TodoInput />
-      <TodoButton />
-      <TodoItems />
+      <div className=" bg-slate-800 rounded-3xl flex flex-col items-center justify-center gap-4 px-4 py-4">
+        <TodoInput />
+        <TodoButton />
+        <TodoItems />
+      </div>
     </div>
   );
 };
--- src/components/TodoButton.jsx
+++ src/components/TodoButton.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import TodoInput from "./todoInput";
 const TodoButton = () => {
+  function handleClick(value) {
+    console.log(value);
+  }
   return (
     <>
-      <button className="text-white bg-blue-700 hover:bg-blue-800 px-4 py-2 rounded-full  text-lg font-bold hover:scale-105 transition-all">
+      <button
+        className="text-white bg-blue-700 hover:bg-blue-800 px-4 py-2 rounded-full  text-lg font-bold hover:scale-105 transition-all"
+        onClick={handleClick()}
+      >
         Add
       </button>
     </>
