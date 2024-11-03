746569eb45c08994023c7d518c1f8c548bbe1e76
Nasif Fuad <nasifuad007@gmail.com>
2024-11-03 12:28:28 +0600

    zzzzz

--- src/index.js
+++ src/index.js
@@ -8,5 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <TodoApp />
+    <TodoApp />
+    <TodoApp />
+    <TodoApp />
   </React.StrictMode>
 );
